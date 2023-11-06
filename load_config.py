91c8b3167a9f1d7f16e5df92320b9757c4af7f52
Sam Zaydel <szaydel@gmail.com>
2023-11-06 05:22:55 -0800

    Fixed configuration loading

--- load_config.py
+++ load_config.py
@@ -27,34 +27,68 @@ def load_config(filename: str) -> Tuple[dict[str, Any] | None, None | str]:
         return None, err.strerror
 
 
-def config_to_share_info_list(config: Dict[str, Any]) -> List[ShareInfo]:
-    si_list = []
-    if not config:
-        return []
+def probe_config_to_si(settings: Dict[str, str]) -> ShareInfo:
+    print("settings: ", settings)
+    password: str = settings.get("password")
+    if not password:
+        password = os.environ.get("SMB_MONITOR_PROBE_PASSWORD")
+    else:
+        # We can specify the environment variable from which to obtain the
+        # password by prefixing the name of the variable with `$ENV_`.
+        # If a "password" string with `$ENV_` is found, we will trim off
+        # this prefix and search the environment for a variable with that
+        # name.
+        if password.startswith("$ENV_"):
+            password = os.environ.get(password.removeprefix("$ENV_"))
+    print("PASSWORD:", password)
+    if not password:
+        print("environmentL:", os.environ, flush=True)
+        raise RuntimeError("password is required for basic functionality")
 
-    for _, settings in config.items():
-        password: str = settings.get("password")
-        if not password:
-            password = os.environ.get("SMB_MONITOR_PROBE_PASSWORD", "invalid")
-        else:
-            # We can specify the environment variable from which to obtain the
-            # password by prefixing the name of the variable with `$ENV_`.
-            # If a "password" string with `$ENV_` is found, we will trim off
-            # this prefix and search the environment for a variable with that
-            # name.
-            if password.startswith("$ENV_"):
-                password = os.environ.get(password.removeprefix("$ENV_"), "invalid")
-        if not password:
-            raise RuntimeError("password is required for basic functionality")
-        si_list.append(
-            ShareInfo(
+    return ShareInfo(
                 addr=settings.get("address"),
                 share=settings.get("share"),
                 domain=settings.get("domain"),
                 user=settings.get("username"),
                 passwd=password,
             )
-        )
+
+def config_to_share_info_list(config: Dict[str, Any]) -> List[ShareInfo]:
+    si_list = []
+    if not config:
+        return []
+
+    for key, settings in config.items():
+        print("key =", key, "settings =", settings, flush=True)
+        if key != "probes":
+            continue
+        # print("key =", key, "settings =", settings)
+        for probe in settings:
+            si = probe_config_to_si(probe)
+            print("generated si:", si, flush=True)
+            si_list.append(si)
+        # password: str = settings.get("password")
+        # if not password:
+        #     password = os.environ.get("SMB_MONITOR_PROBE_PASSWORD", "invalid")
+        # else:
+        #     # We can specify the environment variable from which to obtain the
+        #     # password by prefixing the name of the variable with `$ENV_`.
+        #     # If a "password" string with `$ENV_` is found, we will trim off
+        #     # this prefix and search the environment for a variable with that
+        #     # name.
+        #     if password.startswith("$ENV_"):
+        #         password = os.environ.get(password.removeprefix("$ENV_"), "invalid")
+        # if not password:
+        #     raise RuntimeError("password is required for basic functionality")
+        # si_list.append(
+        #     ShareInfo(
+        #         addr=settings.get("address"),
+        #         share=settings.get("share"),
+        #         domain=settings.get("domain"),
+        #         user=settings.get("username"),
+        #         passwd=password,
+        #     )
+        # )
     return si_list
 
 
